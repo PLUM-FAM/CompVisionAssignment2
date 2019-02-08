1c9fa45b919eafe48afe59c3f0ae3d912a152d0e
joel <hedwigg01@gmail.com>
2019-02-08 12:57:54 -0700

    test program for open cv

--- assignment2.py
+++ assignment2.py
@@ -1,1 +1,11 @@
-print("test")
+import cv2
+cap = cv2.VideoCapture(0)
+cv2.namedWindow("Video")
+while True:
+    status, img = cap.read()
+    cv2.imshow("Video", img)
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
+cv2.destroyAllWindows()
+print("windows closed")
