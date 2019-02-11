de0f8ac4cea9d8012a70320173165be2570f49d2
joel <hedwigg01@gmail.com>
2019-02-11 14:18:16 -0700

    Formatting and cleanups for part 1

--- assignment2.py
+++ assignment2.py
@@ -1,16 +1,20 @@
 import cv2
 import numpy
 
+#global window height and width for every drawn video window.
+windowHeight = 500
+windowWidth = 800
+
 #numpy array order: hue, saturation, value
 mins = numpy.zeros(3)
 maxs = numpy.zeros(3)
 
-
+#onclick event function for hsv window
 def onClick(event,x,y,flags,param):
         if event == cv2.EVENT_LBUTTONDOWN:
                 print(hsv[y,x]) #print out hsv values for current click x,y coords
 
-#adjust mins
+#adjust mins functions
 def adjust_hue_MIN(value):
         mins[0] = value
 
@@ -20,7 +24,7 @@ def adjust_sat_MIN(value):
 def adjust_val_MIN(value):
         mins[2] = value
 
-#adjust maxs
+#adjust maxs functions
 def adjust_hue_MAX(value):
         maxs[0] = value
 
@@ -31,9 +35,7 @@ def adjust_val_MAX(value):
         maxs[2] = value
 
 
-
-
-
+#capture camera and create 3 windows for part 1
 cap = cv2.VideoCapture(0)
 cv2.namedWindow("hsv",0)
 cv2.namedWindow("Video",0)
@@ -50,19 +52,12 @@ cv2.createTrackbar('Sat MAX','hsv',0,255,adjust_sat_MAX)
 cv2.createTrackbar('Val MIN','hsv',0,255,adjust_val_MIN)
 cv2.createTrackbar('Val MAX','hsv',0,255,adjust_val_MAX)
 
-
-
-
-
-
-
-
+#process each frame
 while True:
         status, img = cap.read()
 
         #display normal video window
         cv2.imshow("Video", img)
-
 
         #convert img to HSV and save to hsv
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -87,8 +82,6 @@ while True:
         #display final object tracking video window (erosion is after dialating the og grayscale image and then eroding it)
         cv2.imshow("track", afterErosion)
 
-        windowHeight = 500
-        windowWidth = 800
         cv2.resizeWindow('Video', windowWidth,windowHeight)
         cv2.resizeWindow('hsv', windowWidth,windowHeight)
         cv2.resizeWindow('track', windowWidth,windowHeight)
@@ -97,7 +90,6 @@ while True:
         if k == 27:
                 break
 
-    
 
 cv2.destroyAllWindows()
 print("windows closed")
